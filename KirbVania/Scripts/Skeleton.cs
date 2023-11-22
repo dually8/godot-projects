9798762c2fdd74ffe66d877cfb0630ddc7660577
CJ Coffey <cj@cjcoffey.com>
2023-11-22 07:15:38 -0500

    add enemy ai
    and by ai i mean "move left to right"

--- KirbVania/Scripts/Skeleton.cs
+++ KirbVania/Scripts/Skeleton.cs
@@ -7,6 +7,9 @@ public partial class Skeleton : CharacterBody2D
 	private AnimatedSprite2D _sprite;
 	private Vector2 _walkingDirection = Vector2.Left;
 	private float _speed = 50.0f;
+	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	[Export] private RayCast2D _rayCast2D;
+	[Export] private float _rayCastLength = 16.0f;
 
 	public override void _Ready()
 	{
@@ -14,6 +17,7 @@ public partial class Skeleton : CharacterBody2D
 		_sprite = GetNode<AnimatedSprite2D>("Sprite");
 		_sprite.AnimationFinished += OnAnimatedFinished;
 		_sprite.Play(SkeletonAnimations.Walk);
+		_rayCast2D.TargetPosition = new Vector2(0.0f, _rayCastLength);
 		if (GetTree().GetNodesInGroup("Player").FirstOrDefault() is Player player)
 		{
 			_walkingDirection = (player.Position - Position).Normalized();
@@ -22,8 +26,27 @@ public partial class Skeleton : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Velocity = _walkingDirection * _speed;
+		// Apply gravity
+		var velocity = Velocity;
+		if (!IsOnFloor())
+			velocity.Y += _gravity * (float)delta;
+		velocity.X = _walkingDirection.X * _speed;
+		HandleFacingDirection(velocity);
+		Velocity = velocity;
+		SnapToFloor();
 		MoveAndSlide();
+	}
+
+	private void HandleFacingDirection(Vector2 velocity)
+	{
+		if (velocity.X < 0)
+		{
+			_sprite.FlipH = false;
+		}
+		else if (velocity.X > 0)
+		{
+			_sprite.FlipH = true;
+		}
 	}
 
 	private void OnAnimatedFinished()
@@ -31,6 +54,16 @@ public partial class Skeleton : CharacterBody2D
 		if (_sprite.Animation == SkeletonAnimations.Die)
 		{
 			QueueFree();
+		}
+	}
+
+	private void SnapToFloor()
+	{
+		if (!IsOnFloor() && _rayCast2D.IsColliding())
+		{
+			Vector2 groundPosition = _rayCast2D.GetCollisionPoint();
+			// Position will snap to location just above the raycast
+			Position = new Vector2(Position.X, groundPosition.Y - _rayCastLength);
 		}
 	}
 
